05082adad777f69d99be3db4eff3fdcacdb1f855
João Sena <joaopedromsena@gmail.com>
2022-03-07 15:31:55 -0300

    Adicionando código refatorado do módulo 'Object'

--- src/Object.hpp
+++ src/Object.hpp
@@ -28,24 +28,4 @@ private:
 	BITMAP*	m_pbmp;
 };
 
-Object::Object(const char* _name)
-{
-	m_pbmp = load_bitmap(_name, nullptr);
-}
-
-Object::~Object()
-{
-	destroy_bitmap(m_pbmp);
-}
-
-void Object::draw()
-{
-	draw_sprite(TELA, m_pbmp, rect.x, rect.y);
-}
-
-Rect& Object::get_rect()
-{
-	return m_rect;
-}
-
 #endif /* OBJECT_HPP */
