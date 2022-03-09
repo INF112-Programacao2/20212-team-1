6e5ef3cf80c078b150ee002ffca4155ef6b3fe2a
João Pedro Medrado Sena <83306527+jpmedras@users.noreply.github.com>
2022-03-09 15:29:50 -0300

    Alterando importações

--- src/player.hpp
+++ src/player.hpp
@@ -4,8 +4,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
-#include "personagem.hpp"
-#include "object.hpp"
+#include "Personagem.hpp"
+#include "Object.hpp"
 #include "Position.hpp"
 
 class Player : public Personagem{
