689de142e7270cc01a82033611dca5ea38cb9a5c
João Sena <joaopedromsena@gmail.com>
2022-03-11 09:38:32 -0300

    Removendo importações duplicadas

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-
 #include "Personagem.hpp"
-#include "Object.hpp"
-#include "Position.hpp"
 
 Personagem::Personagem(std::string name, ALLEGRO_BITMAP *image,int x, int y):
     _name(name), Object(image, x, y){};
--- src/Personagem.hpp
+++ src/Personagem.hpp
@@ -1,9 +1,6 @@
 #ifndef INF112_PERSONAGEM_HPP
 #define INF112_PERSONAGEM_HPP
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-
 #include "Position.hpp"
 #include "Object.hpp"
 
