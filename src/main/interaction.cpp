63578171966e384e0f0f5e155355ef25118b4903
João Sena <joaopedromsena@gmail.com>
2022-03-13 10:57:38 -0300

    Adicionando documentação ao módulo 'Interaction'

--- src/main/interaction.cpp
+++ src/main/interaction.cpp
@@ -1,13 +1,16 @@
-#include "Interaction.hpp"
+#include "../Interaction.hpp"
 
 #include <iostream>
 
 int main() {
-	Interaction it("file/dialogs.txt");
-	for (int i = 0; i <it.get_dialog_length(); i++) {
+	Interaction it("file/Andre.txt");
+	int num_dialogs = it.get_dialog_length();
+	std::cout << "There are " << num_dialogs << " dialogs\n" << std::endl;
+	for (int i = 0; i < num_dialogs; i++) {
 		std::string* dialog = it.get_dialog(i);
-		std::cout << dialog[it.SPEAK] << std::endl;
-		std::cout << dialog[it.ANSWER] << std::endl;
+		std::cout << "Player: " << dialog[it.SPEAK] << std::endl;
+		std::cout << "NPC: " << dialog[it.ANSWER] << std::endl;
+		std::cout << "***" << std::endl;
 	}
 	return 0;
 }
