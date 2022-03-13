22a95faeb5a99eb5fec7587a172eec8d6691a705
João Sena <joaopedromsena@gmail.com>
2022-03-13 10:59:50 -0300

    Adicionando get_name()

--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -4,3 +4,7 @@ Personagem::Personagem(std::string name, ALLEGRO_BITMAP *image,int x, int y):
     _name(name), Object(image, x, y) {};
 
 Personagem::~Personagem() {}
+
+std::string Personagem::get_name() {
+	return this->_name;
+}
--- src/Personagem.hpp
+++ src/Personagem.hpp
@@ -7,12 +7,13 @@
 class Personagem : public Object{
     protected:
       std::string _name;
+      Position _position;
       // TODO: Adicionar vetor de Capimons
       //vector<*Capimon> _deck
     public:
     	// TODO: Incluir deck
 		  Personagem(std::string name, ALLEGRO_BITMAP *image,int x, int y);
 		  ~Personagem();
-
+		  std::string get_name();
 };
 #endif
