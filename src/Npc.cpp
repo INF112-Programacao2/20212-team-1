692acb5227e5364fe2902c1c357f57edc67c547c
João Sena <joaopedromsena@gmail.com>
2022-03-18 21:03:19 -0300

    Excluindo linha

--- src/Npc.cpp
+++ src/Npc.cpp
@@ -6,7 +6,6 @@
 
 Npc::Npc(std::string name, ALLEGRO_BITMAP *image, int x, int y, std::string file_directory[]):
 	Personagem(name, image, x, y) {
-	this->_dialog_image = dialog_image;
 	for (int i = 0; i < 1; i++) {
 		std::string t = file_directory[i];
 		Interaction *interaction = new Interaction(t);
