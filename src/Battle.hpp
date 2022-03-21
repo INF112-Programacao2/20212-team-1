f55e32914d19beda6815272cba4c618b4fb9bee6
João Sena <joaopedromsena@gmail.com>
2022-03-21 18:05:48 -0300

    Adicionando importações e constantes de tela

--- src/Battle.hpp
+++ src/Battle.hpp
@@ -2,6 +2,21 @@
 
 #include "Npc.hpp"
 
+// TODO: Check if there is something repeated
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <iostream>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include "CapimonStatus.hpp"
+#include "Capimon.hpp"
+#include "PlayerAttack.hpp"
+
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+
 class Battle {
 	private:
 		Npc *_enemy;
