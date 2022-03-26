175e6145f3d420caf70b2acf54000a72e1b9084f
SRaimundo <samuel.r.pinto@ufv.br>
2022-03-26 17:32:14 -0300

    adicionando funcao em Capimon

--- src/Capimon.cpp
+++ src/Capimon.cpp
@@ -2,7 +2,9 @@
 
 #include "Capimon.hpp"
 
-Capimon::Capimon(string name, ALLEGRO_BITMAP *image, int life, Skill skills[]){
+int Capimon::AMOUNT_SKILLS = 4;
+
+Capimon::Capimon(string name, ALLEGRO_BITMAP *image, int life, Skill skills[AMOUNT_SKILLS]){
 	this->_name = name;
 	this->_image = image;
 	this->_life = life;
@@ -12,7 +14,7 @@ Capimon::Capimon(string name, ALLEGRO_BITMAP *image, int life, Skill skills[]){
 }
 
 Capimon::~Capimon(){
-    al_destroy_bitmap(this->_image);
+    //al_destroy_bitmap(this->_image);
 }
 
 std::string Capimon::get_name() {
@@ -23,7 +25,13 @@ int Capimon::get_lif() {
 	return this->_life;
 }
 
-void Capimon::show_capimon(){
+void Capimon::show_capimonPlayer(){
 	al_convert_mask_to_alpha(this->_image, al_map_rgb(255,0,255));
 	al_draw_bitmap(this->_image, 200, 295, 0); 	//the position is changed if it's a player or and NPC
+}
+
+void Capimon::show_capimonNpc(){
+	al_convert_mask_to_alpha(this->_image, al_map_rgb(255,0,255));
+	al_draw_bitmap(this->_image, 200, 295, 0); 	//the position is changed if it's a player or and NPC 
+	//alterar posicao da imagem
 }
--- src/Capimon.hpp
+++ src/Capimon.hpp
@@ -12,13 +12,14 @@ class Capimon{
     ALLEGRO_BITMAP *_image;
     int _life;
     Skill _skills[AMOUNT_SKILLS];	// Or maybe Skill *_skills[4]
-    const int AMOUNT_SKILLS = 4;
+    static int AMOUNT_SKILLS;
   public:
-		Capimon(std::string name, ALLEGRO_BITMAP *image, int life, Skill skills[]);
+		Capimon(std::string name, ALLEGRO_BITMAP *image, int life, Skill skills[AMOUNT_SKILLS]);
 		~Capimon();
 		std::string get_name();
 		int get_life();
-		void show_capimon();
+		void show_capimonNpc();
+		void show_capimonPlayer();
 };
 
 #endif /* CAPIMON_HPP */
