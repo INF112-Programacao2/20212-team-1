3b9367da807cc492ae80244bf04b3a9a045b7854
João Sena <joaopedromsena@gmail.com>
2022-03-27 19:15:02 -0300

    Adicionando skills

--- src/main/battle.cpp
+++ src/main/battle.cpp
@@ -1,6 +1,10 @@
 #include "../Battle.hpp"
 
 int main() {
+	Skill choque(0, "Choque do trovão", 10, 20);
+	Skill cauda(1, "Cauda de ferro", 50, 120);
+	Skill pena(2, "Pena de gaivota", 5, 10);
+	Skill copo(3, "Copo de plástico", 175, 200);
 	
 	return 0;
 }
