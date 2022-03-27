d32e9b6eb802348e7fb14979fd9ea2985477acd8
João Sena <joaopedromsena@gmail.com>
2022-03-27 19:20:29 -0300

    Adicionando capimon

--- src/main/battle.cpp
+++ src/main/battle.cpp
@@ -1,10 +1,17 @@
 #include "../Battle.hpp"
 
 int main() {
+	al_init();
+	al_init_image_addon();
+	
+	ALLEGRO_BITMAP *pi("../img/battle/pikachu.bmp")
+	// TODO: Remove background
+	
 	Skill choque(0, "Choque do trovão", 10, 20);
 	Skill cauda(1, "Cauda de ferro", 50, 120);
 	Skill pena(2, "Pena de gaivota", 5, 10);
 	Skill copo(3, "Copo de plástico", 175, 200);
 	
+	Capimon picachu("Picachu", pi, 350, {choque, cauda, pena, copo});
 	return 0;
 }
