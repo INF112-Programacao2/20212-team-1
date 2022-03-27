a0861e98c8e479d73d677eb254a9e65aff458495
João Sena <joaopedromsena@gmail.com>
2022-03-27 08:49:16 -0300

    Alterando posição dos atributos

--- src/Skill.hpp
+++ src/Skill.hpp
@@ -4,12 +4,12 @@
 
 class Skill {
 	private:
-		int _index;
-		std::string _name;
-		int _damage_interval[2];
 		// TODO: Change position of consts
 		const int MIN_DAMAGE = 0;
 		const int MAX_DAMAGE = 1;
+		int _index;
+		std::string _name;
+		int _damage_interval[2];
 	public:
 		Skill(int index, std::string name, int min_damage, int max_damage);
 		~Skill();
