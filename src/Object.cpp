ec23a93ead616ceb9cf903e38ecf64afb4857e39
João Sena <joaopedromsena@gmail.com>
2022-03-28 08:51:59 -0300

    Adicionando documentação básica

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,17 +1,24 @@
 /* Object.cpp */
 
-#include "Position.hpp"
 #include "Object.hpp"
 
 ALLEGRO_FONT* Object::gblFont = al_load_font("file/fonte.ttf", DEF_FONT_SIZE, 0);
 
 Object::Object(ALLEGRO_BITMAP *image,int x, int y):
-	_position(x,y), _image(image){};
+	_position(x,y), _image(image) {
+		this->_font_directory = "file/font.ttf";
+		this->_font_size = 11;
+		/*
+		TODO:
+		if (alegro is init and addon font is init)
+			this->_font = al_load_font(this->_font_directory.c_str(), this->_font_size, 0);
+		*/
+	};
 
 Object::~Object(){}
 
 void Object::change_image(ALLEGRO_BITMAP *image){
-	_image = image;
+	this->_image = image;
 }
 
 Position Object::get_position() {
--- src/Object.hpp
+++ src/Object.hpp
@@ -1,26 +1,33 @@
 /* Object.hpp */
 
+/*
+ * This class represent everything that has a "fisical" image in the game
+ */
+ 
+
 #ifndef OBJECT_HPP
 #define OBJECT_HPP
+
+#include "Position.hpp"
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
-#include "Position.hpp"
-
-#define DEF_FONT_SIZE	12
+#include <string>
 
 class Object {
 	protected:
 		ALLEGRO_BITMAP *_image;
 		Position _position;
 	public:
-		static ALLEGRO_FONT* gblFont;
-
+		std::string _font_directory;
+		ALLEGRO_FONT* _font;
+		int _font_size;
 		Object(ALLEGRO_BITMAP *image, int x, int y);
 		~Object();
 		void change_image(ALLEGRO_BITMAP *image);
+		ALLEGRO_BITMAP* get_image();
 		Position get_position();
 };
 
