6969cd674f6324d41b279855c092b5d8b49a9e54
João Sena <joaopedromsena@gmail.com>
2022-03-28 09:28:22 -0300

    Alterando atributos

--- src/Skill.hpp
+++ src/Skill.hpp
@@ -11,14 +11,14 @@
 
 class Skill {
 	private:
-		const int MIN_DAMAGE = 0;
-		const int MAX_DAMAGE = 1;
 		int _index;
 		std::string _name;
 		int _damage_interval[2];
 	public:
 		Skill(int index, std::string name, int min_damage, int max_damage);
 		~Skill();
+		const int MIN_DAMAGE = 0;
+		const int MAX_DAMAGE = 1;
 		int get_index();
 		std::string get_name();
 		int select_damage();
