1f6b012b1f32d0bcce8b2d988ec5e50ab79d3cff
João Sena <joaopedromsena@gmail.com>
2022-03-28 08:41:37 -0300

    Adicioando documentação básica

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,11 +1,8 @@
 /* Player.cpp */
 
-#include <iostream>
-
 #include "Player.hpp"
 
-int Player::_numSprites = 4;
-int Player::_tamPixels = 16;
+#include <iostream>
 
 Player::Player(std::string name, ALLEGRO_BITMAP *image,int x, int y, int tam_x, int tam_y):
     Character(name, image,x, y), _tam_x(tam_x), _tam_y(tam_y),_direcao_anterior(0), _numPasso(0) {};
@@ -16,27 +13,29 @@ Player::~Player(){}
 bool Player::walk(int direcao, Map &mapa){
     bool andou = false;
     if(direcao == -1){
-        al_draw_bitmap_region(_image, 0, 0,this->_tam_x,this->_tam_y,_position.get_x() * _tamPixels,_position.get_y() * _tamPixels,0 );
+        al_draw_bitmap_region(_image, 0, 0, this->_tam_x, this->_tam_y,_position.get_x() * _tamPixels,_position.get_y() * _tamPixels, 0);
         _numPasso = 0;
         this->_direcao_anterior = 0;
 
-    }else{
-        if(direcao == 0 && mapa.get_val_walkable(this->_position.get_x(), this->_position.get_y() + 1) == '1'){
+    }
+    else {
+        if(direcao == 0 && mapa.get_val_walkable(this->_position.get_x(), this->_position.get_y() + 1) == '1') {
             this->_position.add_y();
             andou = true;
-        }else if(direcao == 1 && mapa.get_val_walkable(this->_position.get_x(), this->_position.get_y() - 1) == '1'){
+        } else if(direcao == 1 && mapa.get_val_walkable(this->_position.get_x(), this->_position.get_y() - 1) == '1') {
             this->_position.sub_y();
             andou = true;
-        }else if(direcao == 2 && mapa.get_val_walkable(this->_position.get_x() - 1, this->_position.get_y() ) == '1'){
+        } else if(direcao == 2 && mapa.get_val_walkable(this->_position.get_x() - 1, this->_position.get_y() ) == '1') {
             this->_position.sub_x();
             andou = true;;
-        }else if(direcao == 3 && mapa.get_val_walkable(this->_position.get_x() + 1, this->_position.get_y() ) == '1'){
+        } else if(direcao == 3 && mapa.get_val_walkable(this->_position.get_x() + 1, this->_position.get_y() ) == '1') {
             this->_position.add_x();
             andou = true;
         }
+        
         if(this->_direcao_anterior == direcao){
             this->_numPasso++;
-            if(this->_numPasso >= _numSprites )
+            if(this->_numPasso >= this->_numSprites )
                 this->_numPasso = 0;
 
         }else{
@@ -44,16 +43,16 @@ bool Player::walk(int direcao, Map &mapa){
         }
         this->_direcao_anterior = direcao;
 
-        al_draw_bitmap_region(_image, this->_numPasso * this->_tam_x, direcao * this->_tam_y,this->_tam_x,this->_tam_y,_position.get_x() * _tamPixels,_position.get_y() * _tamPixels,0 );
+        al_draw_bitmap_region(_image, this->_numPasso * this->_tam_x, direcao * this->_tam_y,this->_tam_x, this->_tam_y,_position.get_x() * this->_tamPixels,_position.get_y() * this->_tamPixels, 0);
     }
 
     return andou;
-    
 }
 
 // TODO: Correct interact()
 void Player::interact(Npc *npc) {
 	al_install_keyboard();	// TODO: Verify if it's not duplicate
+	// TODO: Add excepction if keyboard wasn't installed
 	
 	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();	// TODO: Verify if it's not duplicate
 	al_register_event_source(event_queue, al_get_keyboard_event_source());	// TODO: Verify if it's not duplicate
@@ -64,18 +63,18 @@ void Player::interact(Npc *npc) {
 	// TODO: Include time to press the key
 	if(event.type == ALLEGRO_EVENT_KEY_DOWN) {
 		switch (event.keyboard.keycode) {
-			case ALLEGRO_KEY_I:	// The setted key is "N" (from word "next")
+			case ALLEGRO_KEY_I:
 				if (npc->can_interact(this->_position)) {
 					npc->show_interaction();
-					std::cout << "Successful interaction.\n";
+					std::cout << "Successful interaction.\n";	// TODO: Add exception
 				}
 				else {
-					std::cerr << "Error in interaction.\n";
+					std::cerr << "Error in interaction.\n";	// TODO: Add exception
 					return ;
 				}
 				break;
 			default:
-				std::cerr << "Interaction wasn't called.\n";
+				std::cerr << "Interaction wasn't called.\n";	// TODO: Add exception
 				break;
 		}
 	}
--- src/Player.hpp
+++ src/Player.hpp
@@ -1,30 +1,35 @@
 /* Player.hpp */
 
+/*
+ * This class represent the player of the game, allowing the user to control the position of the play with the keyboard and make interactions with NPC's
+ */
+
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
-#include "Object.hpp"
 #include "Position.hpp"
+#include "Object.hpp"
 #include "Map.hpp"
 #include "Character.hpp"
 #include "Npc.hpp"
 
 class Player : public Character{
   private:
-	const Position CAPIMON_DRAW_POSITION = Position(200, 295);
-	const Position CAPIMON_STATUS_POSITION = Position(420, 350);
-	static int _numSprites;
-	static int _tamPixels;
-	int _direcao_anterior;
-	int _numPasso;
-	int _tam_x;
-	int _tam_y;
+		const Position CAPIMON_DRAW_POSITION = Position(200, 295);
+		const Position CAPIMON_STATUS_POSITION = Position(420, 350);
+		const int _numSprites = 4;
+		const int _tamPixels = 16;
+		int _direcao_anterior;
+		int _numPasso;
+		int _tam_x;
+		int _tam_y;
   public:
-    Player(std::string name, ALLEGRO_BITMAP *image,int x, int y, int tam_x, int tam_y);
+  	Player(std::string name, ALLEGRO_BITMAP *image,int x, int y, int tam_x, int tam_y);
     ~Player();
     bool walk(int direcao, Map &mapa);
     void interact(Npc *npc);
-	virtual Position get_CAPIMON_DRAW_POSITION();
+		virtual Position get_CAPIMON_DRAW_POSITION();
     virtual Position get_CAPIMON_STATUS_POSITION();
 };
+
 #endif /* PLAYER_HPP */
