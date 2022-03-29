62f320c7eeec425e8fdb21aaa82d2ab8ceba1407
João Sena <joaopedromsena@gmail.com>
2022-03-29 09:24:07 -0300

    Alterando Dialog

--- src/Dialog.cpp
+++ src/Dialog.cpp
@@ -2,11 +2,6 @@
 
 #include "Dialog.hpp"
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-
 #include <iostream>
 #include <exception>
 
