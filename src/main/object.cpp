0d3155773dbe083b51b138b19d43c7040865f955
SRaimundo <samuel.r.pinto@ufv.br>
2022-03-29 21:07:13 -0300

    Excluindo pasta main

--- src/main/object.cpp
+++
@@ -1,8 +0,0 @@
-#include "../Object.hpp"
-#include <iostream>
-
-int main() {
-	Object ob(nullptr, 10, 10);
-	Position p = ob.get_position();
-	std::cout << p.get_x() << " " << p.get_y() << std::endl;
-}
